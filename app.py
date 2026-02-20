bc8a5368e5fc7c13972312d52e3c6cbba5f29a9b
dkim7890 <dkim7890@terpmail.umd.edu>
2026-02-20 13:49:57 -0500

    about.html app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from David Kim! This is my first code change.'
+    return render_template('about.html')
 
 @app.route('/hello')
 def hello():  # put application's code here
