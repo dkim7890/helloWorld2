b7de34ccea6f51d4feed92eb944de58527690c6e
dkim7890 <dkim7890@terpmail.umd.edu>
2026-03-13 15:00:51 -0400

    contact.html

--- app.py
+++ app.py
@@ -1,7 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/favourite-course')
+def favourite_course():  # put application's code here
+    course = request.args.get('course')
+    course_number = request.args.get('course_number')
+    return render_template(
+'favourite-course.html',
+                    course = course,
+                    course_number = course_number)
+
+@app.route('/favorite-course')
+def favorite_course():
+    subject = request.args.get('subject')
+    course_number = request.args.get('course_number')
+    return render_template('favorite-course.html', subject=subject, course_number=course_number)
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        email = request.form.get('email')
+        class_year = request.form.get('class_year')
+
+        return render_template(
+            'contact.html',
+            submitted = True,
+            first_name = first_name,
+            last_name = last_name,
+            email = email,
+            class_year = class_year
+        )
+
+    return render_template('contact.html', submitted=False)
 
 @app.route('/')
 def hello_world():  # put application's code here
